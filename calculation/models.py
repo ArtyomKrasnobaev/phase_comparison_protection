68ce7cc669905f02e58213829b3ce48d4c57d704
ArtyomKrasnobaev <artyom-red@yandex.ru>
2024-11-18 21:17:06 +0700

    добавлена модель CalculationProtocol (calculation)

--- calculation/models.py
+++ calculation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from core.models import Line, Component
+
+class CalculationProtocol(models.Model):
+    """Модель протокола расчета параметров настройки ДФЗ."""
+
+    line = models.ForeignKey(Line, on_delete=models.CASCADE)
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    condition = models.CharField(verbose_name='Расчетное условие', max_length=100)
+    result_value = models.FloatField(verbose_name='Уставка')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Мета-данные модели CalculationProtocol."""
+
+        verbose_name = 'Протокол расчета'
+        verbose_name_plural = 'Протоколы расчетов'
